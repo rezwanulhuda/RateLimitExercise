75ec5d4ae9cefa6db7afb7ff1c772307407bac03
Rezwanul Huda <rezwanul.huda@infolytx.com>
2016-11-22 21:00:22 +0600

    Initial version added

---
+++ Lib.Tests/LimitCounterTest.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+using System;
+namespace Lib.Tests
+{
+	[TestFixture()]
+	public class LimitCounterTest
+	{
+		[Test()]
+		public void CurrentCount_Returns0_WhenCalledAfterInitializing()
+		{
+			int nrOfRequests = 0;
+			int allowedTime = 0;
+			int secondsSuspended = 0;
+
+			//nrOfRequests, seconds, minutesSuspended
+			LimitCounter counter = new LimitCounter(nrOfRequests, TimeSpan.FromSeconds(allowedTime), TimeSpan.FromSeconds(secondsSuspended));
+
+
+			Assert.AreEqual(0, counter.CurrentCount);
+
+
+		}
+
+		[Test()]
+		public void Increase_IncreasesCounterBy1()
+		{
+			int nrOfRequests = 1;
+			int allowedTime = 1;
+			int secondsSuspended = 5;
+
+			//nrOfRequests, seconds, minutesSuspended
+			LimitCounter counter = new LimitCounter(nrOfRequests, TimeSpan.FromSeconds(allowedTime), TimeSpan.FromSeconds(secondsSuspended));
+
+			counter.Increase();
+			Assert.AreEqual(1, counter.CurrentCount);
+
+
+		}
+
+		[Test()]
+		public void Increase_WhenLimitExceeds_ThrowsException()
+		{
+			int nrOfRequests = 1;
+			int allowedTime = 1;
+			int secondsSuspended = 5;
+
+			//nrOfRequests, seconds, minutesSuspended
+			LimitCounter counter = new LimitCounter(nrOfRequests, TimeSpan.FromSeconds(allowedTime), TimeSpan.FromSeconds(secondsSuspended));
+
+			counter.Increase();
+			Assert.AreEqual(1, counter.CurrentCount);
+			try
+			{
+				counter.Increase();
+				Assert.Fail("Did not throw exception");
+			}
+			catch (NUnit.Framework.AssertionException e)
+			{
+				throw e;
+			}
+			catch (Exception ex)
+			{
+				
+				Assert.AreEqual(String.Format("Limit of {0} requests exceeded.", 1), ex.Message);
+			}
+		}
+
+		[Test()]
+		public void Increase_WhenLimitExceeds_AllowsRequestAfterElapsedTime()
+		{
+			int nrOfRequests = 1;
+			int allowedTime = 1;
+			int secondsSuspended = 1;
+
+			//nrOfRequests, seconds, minutesSuspended
+			LimitCounter counter = new LimitCounter(nrOfRequests, TimeSpan.FromSeconds(allowedTime), TimeSpan.FromSeconds(secondsSuspended));
+
+			counter.Increase();
+			System.Threading.Thread.Sleep(secondsSuspended * 2 * 1000);
+			counter.Increase();
+		}
+
+		[Test()]
+		public void Increase_WhenTimeElapsedAfter_ResetsCounter()
+		{
+			int nrOfRequests = 2;
+			int allowedTime = 2;
+			int secondsSuspended = 1;
+
+			//nrOfRequests, seconds, minutesSuspended
+			LimitCounter counter = new LimitCounter(nrOfRequests, TimeSpan.FromSeconds(allowedTime), TimeSpan.FromSeconds(secondsSuspended));
+
+			counter.Increase();
+			System.Threading.Thread.Sleep(2 * 1000);
+			counter.Increase();
+			Assert.AreEqual(1, counter.CurrentCount);
+
+
+
+		}
+	}
+}
---
+++ Lib/LimitCounter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Lib
+{
+	public class LimitCounter
+	{
+		TimeSpan allowedTime;
+		int nrOfRequests;
+		TimeSpan suspendedFor;
+		DateTime firstRequest;
+		public LimitCounter()
+		{
+			this.CurrentCount = 0;
+
+		}
+
+		public int CurrentCount { get; private set; }
+
+		public LimitCounter(int nrOfRequests, TimeSpan allowedTime, TimeSpan suspendFor)
+		{
+			this.nrOfRequests = nrOfRequests;
+			this.allowedTime = allowedTime;
+			this.suspendedFor = suspendFor;
+		}
+
+
+
+		public void Increase()
+		{
+			if (this.CurrentCount == 0)
+			{
+				firstRequest = DateTime.Now;
+			}
+
+			if (this.CurrentCount >= this.nrOfRequests)
+			{
+				var diff = DateTime.Now.Subtract(firstRequest).Seconds;
+				var next = allowedTime.Add(this.suspendedFor).Seconds;
+				if (diff >= next)
+				{
+					this.CurrentCount = 0;
+					firstRequest = DateTime.Now;
+				}
+				else 
+				{
+					throw new Exception(String.Format("Limit of {0} requests exceeded.", this.nrOfRequests));
+				}
+
+
+			}
+				
+
+
+			this.CurrentCount++;
+		}
+
+
+
+	}
+}
